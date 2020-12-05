d0a8788ce29e5ace66e6076a1350c064be595c6a
Jericho Fillon <fillonjericho@gmail.com>
2020-12-05 20:10:51 +0000

    Declaring and Calling functions

---
+++ hello.js
@@ -0,0 +1,14 @@
+const sayHelloToConsole = function(name){
+  console.log("Hello, " + name + "!");
+}
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name + "!";
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+
+
+sayHelloToConsole("Caliban");
+sayHelloToConsole("Miranda");
+sayHelloToConsole("Ferdinand");
